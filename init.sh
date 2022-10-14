932521620cf4c07f68e33a03b799d073061cc6a3
Юлия Худеньких <71973418+Yulia91025@users.noreply.github.com>
2022-10-14 21:38:03 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,3 +3,4 @@ sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test-wsgi
 sudo /etc/init.d/gunicorn restart
+gunicorn -w 2 -c /home/box/web/etc/hello.py hello:app & gunicorn -w 2 -c /home/box/web/etc/qa.py ask.wsgi:application
