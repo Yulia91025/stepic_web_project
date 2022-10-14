db0edffdd5cf9bf30ad0f021af3c524d4a7c5771
Юлия Худеньких <71973418+Yulia91025@users.noreply.github.com>
2022-10-14 21:53:56 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-gunicorn -w 2 -c /home/box/web/etc/hello.py hello:app & gunicorn -w 2 -c /home/box/web/etc/qa.py ask.wsgi:application
+gunicorn -w 2 -c /home/box/web/etc/hello.py hello:wsgi_application & gunicorn -w 2 -c /home/box/web/etc/qa.py ask.wsgi:application
