db90752658759bf11cd3a7de1aea37eed0158d7c
Юлия Худеньких <71973418+Yulia91025@users.noreply.github.com>
2022-10-21 13:03:15 +0300

    Update models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class QuestionManager(models.Model):
+class QuestionManager(models.Manager):
     def new(self):
         return self.order_by('-added_at')
     def popular(self):
